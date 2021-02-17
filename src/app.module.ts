51cd74080430795481a240cdf3d02724f641930e
Maxim Vahtel <maxim.vahtel@amvonet.com>
2021-02-17 18:46:13 +0400

    connet to empty db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      retryAttempts: 2,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
